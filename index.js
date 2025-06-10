64013953a7d8114cf2f8467889f94832dc6e5d28
arafat22184 <123alarafat@gmail.com>
2025-06-10 15:35:26 +0600

    successfully connected to mongodb server

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+
+// MONGODB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydu4ilk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const blogify = client.db("blogify");
+    const blogsCollection = blogify.collection("blogs");
+
+    // CRUD
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Blogify Running");
+});
+
+app.listen(port, () => {
+  console.log(`Blogify running on port ${port}`);
+});
